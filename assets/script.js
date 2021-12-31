c9ba613682e65bdb808bc462c63982965440597e
tooqk4u <tooqk4u@protonmail.com>
2021-12-31 14:47:56 -0500

    fixed javascrit file and added HTML

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+let fiveDay = [{
+    date: getDate(),
+    icon: "",
+    temp: "",
+    humidity: "",
+}]
+
+
+ append.container `<div class="fiveDay rows" style= "border: 5px solid white">
+    <p class="fiveDay-date">Date</p>  
+    <p class="fiveDay-condition"></p>        
+    <p class="fiveDay-temp">Temp:</p>
+    <p class="fiveDay-hum">Humidity:</p>         
+  </div>`
