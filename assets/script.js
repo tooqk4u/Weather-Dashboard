bace2fb50e57a6227a7bb815b11a78d780b25d23
tooqk4u <tooqk4u@protonmail.com>
2022-01-01 16:28:43 -0500

    added javascript for city search button

--- assets/script.js
+++ assets/script.js
@@ -11,3 +11,22 @@ let currentTime = moment().format("dddd MMMM, Do, YYYY, h:mm A");
 let showTime = document.getElementById("currentDay");
 showTime.innerHTML = currentTime;
 console.log(currentTime);
+
+// search submission listener
+$("#search-container").on("submit", citySubmitHandler);
+
+
+// function for city search
+var citySubmitHandler = function (event) {
+  event.preventDefault();
+  var city = $("#searchBar").val().trim();
+  if (city) {
+    getWeatherData(city);
+    $("#searchBar").val("");
+  } else {
+    alert("Please enter a city.");
+  }
+};
+
+
+
