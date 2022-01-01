6519f0bd81759a6854bc60ad200aebce02b5c369
tooqk4u <tooqk4u@protonmail.com>
2022-01-01 09:58:18 -0500

    fixed javascript link and added date/time function

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,13 @@
-let fiveDay = [{
-    date: getDate(),
-    icon: "",
-    temp: "",
-    humidity: "",
-}]
+// set global variables
+let appId = "6afefe490042d7fc49826f4e6606e420";
+let units = "imperial";
+let tempUnitDisplay = "°F";
+let speedUnitDisplay = "MPH";
+let dateFormat = "MM/DD/YYYY";
+let fiveDateFormat = "dddd MM/DD YYYY";
+let cityHistory = [];
 
-
- append.container `<div class="fiveDay rows" style= "border: 5px solid white">
-    <p class="fiveDay-date">Date</p>  
-    <p class="fiveDay-condition"></p>        
-    <p class="fiveDay-temp">Temp:</p>
-    <p class="fiveDay-hum">Humidity:</p>         
-  </div>`
+let currentTime = moment().format("dddd MMMM, Do, YYYY, h:mm A");
+let showTime = document.getElementById("currentDay");
+showTime.innerHTML = currentTime;
+console.log(currentTime);
